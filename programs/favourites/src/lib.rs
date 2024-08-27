75c06345154ed0bd948d96839e941714e002928f
Mateusz Zając <matzayonc@gmail.com>
2024-08-27 20:02:33 +0200

    first context

--- programs/favourites/src/lib.rs
+++ programs/favourites/src/lib.rs
@@ -1,5 +1,7 @@
 use anchor_lang::prelude::*;
 
+pub const ANCHOR_DISCRIMINATOR_SIZE: usize = 8;
+
 declare_id!("7eLAo756veqmTo4192BLmjTW3pfeZMQYjis1zU6SLX64");
 
 #[program]
@@ -13,4 +15,18 @@ pub mod favourites {
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(mut)]
+    pub user: Signer<'info>,
+
+    #[account(init, payer = user, space = ANCHOR_DISCRIMINATOR_SIZE + Favourites::INIT_SPACE,
+        seeds = [b"favorites"], bump
+    )]
+    pub favourites: Account<'info, Favourites>,
+
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+#[derive(InitSpace)]
+pub struct Favourites {}
